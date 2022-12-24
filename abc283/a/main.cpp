9fffb51f4143c46ed0e4cfd9d49c428de6f5ed3f
Hiroyuki Tanaka <ht0919@gmail.com>
2022-12-24 21:17:35 +0900

    Add abc283

---
+++ abc283/a/main.cpp
@@ -0,0 +1,7 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  int A,B; cin>>A>>B;
+  printf("%lld\n",(long long int)pow(A,B));
+  return 0;
+}
--- push.sh
+++ push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 S1="abc"
-S2="282"
+S2="283"
 S3=$S1$S2
 if [[ -d ./$S3 ]]; then
     find . -executable -type f -not -path '*.git*' -delete
