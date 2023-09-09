8360ea0725f0155812a686a22026c96d576de4b6
Hiroyuki Tanaka <ht0919@gmail.com>
2023-09-09 21:14:52 +0900

    Add abc319

---
+++ abc319/a/main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  map<string, int> list {
+      {"tourist", 3858},
+      {"ksun48", 3679},
+      {"Benq", 3658},
+      {"Um_nik", 3648},
+      {"apiad", 3638},
+      {"Stonefeang", 3630},
+      {"ecnerwala", 3613},
+      {"mnbvmar", 3555},
+      {"newbiedmy", 3516},
+      {"semiexp", 3481}
+  };
+  string S; cin>>S;
+  auto iter = list.find(S);
+  cout << iter->second << endl;
+  return 0;
+}
