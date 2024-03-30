379afa822ba03c4dcc90d55a6bb820254e936546
Hiroyuki Tanaka <ht0919@gmail.com>
2024-03-30 21:22:16 +0900

    Add abc347

--- abc347/a/main.cpp
+++ abc347/a/main.cpp
@@ -2,12 +2,13 @@
 using namespace std;
 int main() {
   int N,K; cin>>N>>K;
+  bool first=true;
   for (int i=0,tmp,f=0; i<N; i++) {
     cin>>tmp;
     if ((tmp % K)==0) {
-      if (f!=0 && i<N) cout<<' ';
+      if (first==false && i<N) cout<<' ';
       cout<<(tmp/K);
-      f=1;
+      first=false;
     }
   }
   cout<<endl;
