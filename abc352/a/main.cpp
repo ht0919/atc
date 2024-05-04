795103db4c331e60854298a1d0e190c385bf41a4
Hiroyuki Tanaka <ht0919@gmail.com>
2024-05-04 22:18:43 +0900

    Add abc352

--- abc352/a/main.cpp
+++ abc352/a/main.cpp
@@ -1,15 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define SWAP(a, b) (a ^= b, b = a ^ b, a ^= b)
 int main() {
   int N,X,Y,Z; cin>>N>>X>>Y>>Z;
   string ans="No";
-  if (Y-X>0) { // 登り
-    for (int i=X; i<=Y; i++) {
-      if (i==Z) {ans="Yes"; break;}
-    }
-  } else { // 下り
-    for (int i=X; i>=Y; i--) {
-      if (i==Z) {ans="Yes"; break;}
+  if (Y-X<0) SWAP(X,Y);
+  for (int i=X; i<=Y; i++) {
+    if (i==Z) {
+      ans="Yes";
+      break;
     }
   }
   cout<<ans<<endl;
