03c3180915b7c57ea0423d35975327eca72012ff
Hiroyuki Tanaka <ht0919@gmail.com>
2024-06-15 21:07:00 +0900

    Add abc358

---
+++ abc358/a/main.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  string S,T,ans="No"; cin>>S>>T;
+  if (S=="AtCoder" && T=="Land") {
+    ans = "Yes";
+  }
+  cout<<ans<<endl;
+  return 0;
+}
