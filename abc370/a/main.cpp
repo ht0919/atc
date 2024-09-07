b05e43aa1536e6cb419e98bcd90377f9134dc156
Hiroyuki Tanaka <ht0919@gmail.com>
2024-09-07 21:10:13 +0900

    Add abc370

---
+++ abc370/a/main.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  int L,R; cin>>L>>R;
+  string ans="Invalid";
+  if (L==1 && R==0) ans="Yes";
+  else if (L==0 && R==1) ans="No";
+  cout<<ans<<endl;
+  return 0;
+}
