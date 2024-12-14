30506f7c38fdf5b5feacd36cb3dd54697319c9da
Hiroyuki Tanaka <ht0919@gmail.com>
2024-12-14 21:10:32 +0900

    Add abc384

---
+++ abc384/a/main.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  int N;
+  char c1,c2;
+  string S;
+  cin>>N>>c1>>c2>>S;
+  for (int i=0; i<N; i++) {
+    if (S[i]!=c1) S[i]=c2;
+  }
+  cout<<S<<endl;
+  return 0;
+}
