4dc5b7ea709df52da4d9cb1b2886abded0693af7
Hiroyuki Tanaka <ht0919@gmail.com>
2025-01-11 21:11:17 +0900

    Add abc388

---
+++ abc388/a/main.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  string S; cin>>S;
+  string ans="_UPC";
+  ans[0]=S[0];
+  cout<<ans<<endl;
+  return 0;
+}
--- push.sh
+++ push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 S1="abc"
-S2="387"
+S2="388"
 S3=$S1$S2
 if [[ -d ./$S3 ]]; then
     find . -executable -type f -not -path '*.git*' -delete
