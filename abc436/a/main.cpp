31839af15e2404d870d6525334fee5cd3955c9f0
Hiroyuki Tanaka <ht0919@gmail.com>
2025-12-13 21:28:48 +0900

    Add abc436

--- abc436/a/main.cpp
+++ abc436/a/main.cpp
@@ -1,12 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
-    int N; string S, o = "";
+    int N; string S;
     cin>>N>>S;
     int l = N - S.length();
-    for (int i = 0; i < l; i++) {
-        o += "o";
-    }
+    string o = string(l,'o');
     cout<<o<<S<<endl;
     return 0;
 }
