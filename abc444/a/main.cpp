93dc9b2400963f2983b0f243b4c3769d2da89754
Hiroyuki Tanaka <ht0919@gmail.com>
2026-02-07 21:05:31 +0900

    Add abc444

---
+++ abc444/a/main.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    string N; cin>>N;
+    if (N[0]==N[1] && N[1]==N[2])
+        cout<<"Yes"<<endl;
+    else
+        cout<<"No"<<endl;
+    return 0;
+}
