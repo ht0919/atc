866ce279d226f2bfbb3a733f6b759e9b262627bb
Hiroyuki Tanaka <ht0919@gmail.com>
2026-03-14 21:10:43 +0900

    Add abc449

---
+++ abc449/a/main.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int D; cin>>D;
+    double ans = ((D/2.0)*(D/2.0)*3.141592653589793);
+    printf("%.13f\n", ans);
+    return 0;
+}
