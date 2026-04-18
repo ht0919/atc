fcce21bfda143fb35a1f17ed29a272eccf1f1d66
Hiroyuki Tanaka <ht0919@gmail.com>
2026-04-18 21:09:30 +0900

    Add abc454

---
+++ abc454/a/main.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int L,R; cin>>L>>R;
+    int ans = R-L+1;
+    cout<<ans<<endl;
+    return 0;
+}
--- push.sh
+++ push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 S1="abc"
-S2="453"
+S2="454"
 S3=$S1$S2
 if [[ -d ./$S3 ]]; then
     if [[ "$(uname)" == "Darwin" ]]; then
