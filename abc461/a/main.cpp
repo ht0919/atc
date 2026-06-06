bc9ee502056bffbefecced86bf98a351ae8e8b54
Hiroyuki Tanaka <ht0919@gmail.com>
2026-06-06 21:27:05 +0900

    Add abc461

---
+++ abc461/a/main.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int A,D; cin>>A>>D;
+    string ans="No";
+    if (A <= D) ans="Yes";
+    cout<<ans<<endl;
+    return 0;
+}
--- recov-session/recov-session.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [[ "$(uname)" == "Darwin" ]]; then
-    cp session.json /Users/ht0919/Library/Preferences/atcoder-cli-nodejs
-elif [[ "$(uname)" == "Linux" ]]; then
-    cp session.json ~/.config/atcoder-cli-nodejs/
-else
-    echo "Your platform ($(uname)) is not supportted."
-fi
